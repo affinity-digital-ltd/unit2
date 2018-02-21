eb222503b548581c10ee4a2d12ccedd73a1d138e
Kris Quigley <kris@krisquigley.co.uk>
2018-02-21 16:48:37 +0000

    Added scss

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
 import { Component } from 'react'
 
+import '../scss/application.scss'
+
 class Layout extends Component {
   render () {
-    return <div>
+    return <div className='container'>
       { this.props.children }
     </div>
   }
